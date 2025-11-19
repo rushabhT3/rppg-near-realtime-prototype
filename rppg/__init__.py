b991426975373501f5ce6890b8ec71c900123463
wang <1625298892@qq.com>
2025-11-19 21:45:00 +0800

    Parallel face detection optimized

--- rppg/__init__.py
+++ rppg/__init__.py
@@ -1,6 +1,6 @@
 import os
 #os.dup2(os.open(os.devnull, os.O_WRONLY), 2)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+os.environ['ORT_LOGGING_LEVEL'] = '3'
 from .main import Model 
 from .main import supported_models
